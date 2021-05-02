fa98bce0cd3e756c8a34c388a56c044514273cba
karyasov <karyasov@haulmont.com>
2021-05-02 13:11:18 +0400

    Made some refactoring + added tests

--- service.go
+++ service.go
@@ -9,23 +9,52 @@ import (
 
 // ErrBlocked reports if service is blocked.
 var ErrBlocked = errors.New("blocked")
+var ErrNothingToProcess = errors.New("nothing to process")
+
+const (
+	InputNumberOfElements = "Please input how many elements are you going to process"
+	InputBatchSize        = "Please input batch size"
+	InputTimeout          = "Please input batch timeout"
+)
 
 // Service defines external service that can process batches of items.
 type Service interface {
 	Process(ctx context.Context, batch Batch) error
 }
 
+// BatchService is a struct that encapsulates input parameters
 type BatchService struct {
 	NumberOfElements int
 	BatchSize        int
 	Timeout          int
 }
 
+func setFieldToBatchService(message string, field *int) {
+	fmt.Println(message)
+	for {
+		_, err := fmt.Scan(field)
+		if err != nil {
+			fmt.Println(message)
+		} else {
+			break
+		}
+	}
+}
+
+func MakeNextBatch(items *[]Item, startIndex int, batchService BatchService) []Item {
+	if startIndex+batchService.BatchSize >= batchService.NumberOfElements {
+		return (*items)[startIndex:]
+	}
+	return (*items)[startIndex : startIndex+batchService.BatchSize]
+}
+
 func (bs BatchService) Process(ctx context.Context, batch Batch) error {
+	if len(batch) == 0 {
+		return ErrNothingToProcess
+	}
 	if bs.BatchSize < len(batch) {
 		return ErrBlocked
 	}
-	fmt.Printf("Processing batch with size %d within %d seconds\n", bs.BatchSize, bs.Timeout)
 	fmt.Printf("Processed %d elements\n", len(batch))
 	<-ctx.Done()
 	return nil
@@ -38,19 +67,11 @@ type Batch []Item
 type Item struct{}
 
 func main() {
+	//initializing BatchService structure
 	batchService := BatchService{}
-	for batchService.NumberOfElements == 0 {
-		fmt.Println("Please input how many elements are you going to process")
-		fmt.Scan(&batchService.NumberOfElements)
-	}
-	for batchService.BatchSize == 0 {
-		fmt.Println("Please input batch size")
-		_, _ = fmt.Scan(&batchService.BatchSize)
-	}
-	for batchService.Timeout == 0 {
-		fmt.Println("Please input batch timeout")
-		fmt.Scan(&batchService.Timeout)
-	}
+	setFieldToBatchService(InputNumberOfElements, &batchService.NumberOfElements)
+	setFieldToBatchService(InputBatchSize, &batchService.BatchSize)
+	setFieldToBatchService(InputTimeout, &batchService.Timeout)
 
 	itemSlice := make([]Item, batchService.NumberOfElements)
 	for i := 0; i < batchService.NumberOfElements; i++ {
@@ -60,19 +81,15 @@ func main() {
 	startIndex := 0
 	var batch []Item
 	for startIndex < batchService.NumberOfElements {
-		batch = makeNextBatch(&itemSlice, startIndex, batchService)
-		timeoutCtx, _ := context.WithTimeout(context.Background(), time.Duration(batchService.Timeout)*time.Second)
+		batch = MakeNextBatch(&itemSlice, startIndex, batchService)
 		startIndex += batchService.BatchSize
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Duration(batchService.Timeout)*time.Second)
+		defer cancel()
 		err := batchService.Process(timeoutCtx, batch)
+
 		if err == ErrBlocked {
 			fmt.Println("Could send batch, service was blocked")
 		}
 	}
-}
-
-func makeNextBatch(items *[]Item, startIndex int, batchService BatchService) []Item {
-	if startIndex+batchService.BatchSize >= batchService.NumberOfElements {
-		return (*items)[startIndex:]
-	}
-	return (*items)[startIndex : startIndex+batchService.BatchSize]
 }
---
+++ service_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+type MakeNextBatchTestCase struct {
+	NumberOfElements int
+	BatchSize        int
+	StaringIndex     int
+	ExpectedSize     int
+}
+
+type BatchProcessTestCase struct {
+	MaxBatchSize     int
+	FactualBatchSize int
+	ExpectedError    error
+}
+
+const DefaultTimeout = 1
+
+func TestMakeNextBatch(t *testing.T) {
+	testCases := []MakeNextBatchTestCase{
+		{11, 10, 0, 10},
+		{11, 10, 10, 1},
+		{7, 10, 0, 7},
+		{0, 10, 0, 0},
+	}
+
+	for i, tc := range testCases {
+		testCase := tc
+		t.Run(fmt.Sprintf("test-case-%d", i), func(t *testing.T) {
+			items := make([]Item, testCase.NumberOfElements)
+			for i := range items {
+				items[i] = struct{}{}
+			}
+			batchService := BatchService{len(items), testCase.BatchSize, DefaultTimeout}
+			out := MakeNextBatch(&items, testCase.StaringIndex, batchService)
+			if len(out) != testCase.ExpectedSize {
+				t.Errorf("MakeNextBatch returned length %d, expected %d", len(out), testCase.ExpectedSize)
+			}
+		})
+	}
+}
+
+func TestProcess(t *testing.T) {
+	testCases := []BatchProcessTestCase{
+		{10, 11, ErrBlocked},
+		{10, 0, ErrNothingToProcess},
+		{10, 10, nil},
+		{10, 7, nil},
+	}
+
+	for i, tc := range testCases {
+		testCase := tc
+		t.Run(fmt.Sprintf("test-case-%d", i), func(t *testing.T) {
+			items := make([]Item, testCase.FactualBatchSize)
+			for i := range items {
+				items[i] = struct{}{}
+			}
+			batchService := BatchService{len(items), testCase.MaxBatchSize, DefaultTimeout}
+			timeoutCtx, _ := context.WithTimeout(context.Background(), time.Duration(DefaultTimeout)*time.Second)
+			out := batchService.Process(timeoutCtx, items)
+			if out != testCase.ExpectedError {
+				t.Errorf("expected %s error, got %s", testCase.ExpectedError, out)
+			}
+		})
+	}
+}
